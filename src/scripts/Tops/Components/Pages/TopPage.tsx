f597b1ee7476410ea2e2e8e22ec685db03361218
Tsuyoshi0403 <yasuda_tsuyoshi2002@icloud.com>
2024-08-29 08:33:12 +0900

    [add] ローディングマークを追加

--- src/scripts/Tops/Components/Pages/TopPage.tsx
+++ src/scripts/Tops/Components/Pages/TopPage.tsx
@@ -1,17 +1,24 @@
 import { Route, Routes } from 'react-router-dom'
 import NewLoginPage from './NewLoginPage'
 import MainPage from '../../../Main/Components/Page/MainPage'
+import Loading from '../../../Cores/Components/Organisms/Loading'
+import { useRecoilValue } from 'recoil'
+import LoadingState from '../../../Cores/Recoil/LoadingState'
 
 /**
  * Topコンポーネント
  * @returns {JSX.Element}
  */
 const TopPage = (): JSX.Element => {
+  const isLoading = useRecoilValue(LoadingState)
   return (
-    <Routes>
-      <Route path="/login" element={<NewLoginPage />} />
-      <Route path="/" element={<MainPage />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/login" element={<NewLoginPage />} />
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+      <Loading open={isLoading} />
+    </>
   )
 }
 
